506a7b03fe6abce1eebd9d261bfdde1258a44692
Belkovich Ivan <belkovich.ia@phystech.edu>
2020-11-17 00:42:58 +0300

    Did some changes in solar__model.py

--- solar_model.py
+++ solar_model.py
@@ -33,9 +33,9 @@ def move_space_object(body, dt):
 
     ax = body.Fx/body.m
     ay = body.Fy/body.m
-    body.x += Vx*dt + ax*dt**2/2  
+    body.x += body.Vx*dt + ax*dt**2/2  
     body.Vx += ax*dt
-    body.y += Vy*dt + ay*dt**2/2  
+    body.y += body.Vy*dt + ay*dt**2/2  
     body.Vy += ay*dt
     
 
