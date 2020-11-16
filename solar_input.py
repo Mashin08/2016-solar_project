79d6f4433a6bc4c51638223d65e51dff2dfb2a83
Mashin Nikita <mashin.na@phystech.edu>
2020-11-16 23:32:18 +0300

    Did chsnges in solar_input.py and solar_vis.py

--- solar_input.py
+++ solar_input.py
@@ -23,6 +23,10 @@ def read_space_objects_data_from_file(input_filename):
                 star = Star()
                 parse_star_parameters(line, star)
                 objects.append(star)
+            elif object_type == "planet":
+                planet = Planet()
+                parse_planet_parameters(line, planet)
+                objects.append(planet)  # done the same with planet
             else:
                 print("Unknown space object")
 
@@ -43,9 +47,17 @@ def parse_star_parameters(line, star):
     **line** — строка с описание звезды.
     **star** — объект звезды.
     """
+    line_list = line.split()
 
-    pass  # FIXME: not done yet
-
+    star.R = float(line_list[1])
+    star.color = line_list[2]
+    star.m = float(line_list[3])
+    star.x = float(line_list[4])
+    star.y = float(line_list[5])
+    star.Vx = float(line_list[6])
+    star.Vy = float(line_list[7])
+    
+    
 def parse_planet_parameters(line, planet):
     """Считывает данные о планете из строки.
     Предполагается такая строка:
@@ -61,8 +73,15 @@ def parse_planet_parameters(line, planet):
     **line** — строка с описание планеты.
     **planet** — объект планеты.
     """
-    pass  # FIXME: not done yet...
-
+    line_list = line.split()
+    planet.R = float(line_list[1])
+    planet.color = line_list[2]
+    planet.m = float(line_list[3])
+    planet.x = float(line_list[4])
+    planet.y = float(line_list[5])
+    planet.Vx = float(line_list[6])
+    planet.Vy = float(line_list[7])
+   
 
 def write_space_objects_data_to_file(output_filename, space_objects):
     """Сохраняет данные о космических объектах в файл.
@@ -77,10 +96,13 @@ def write_space_objects_data_to_file(output_filename, space_objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in space_objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
+            print(out_file, obj.type, obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vy)
             # FIXME: should store real values
 
-# FIXME: хорошо бы ещё сделать функцию, сохранающую статистику в заданный файл...
+
+
+
+# FIXME: хорошо бы ещё сделать функцию, соobj.type, obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vyхранающую статистику в заданный файл...
 
 if __name__ == "__main__":
     print("This module is not for direct call!")
